4578d3d3fde3b3f3a9288bacc633c524076347ee
Daprojects1 <93226161+Daprojects1@users.noreply.github.com>
2022-01-07 13:07:41 +0000

    Update mainScript.js
    event listeners added

--- mainScript.js
+++ mainScript.js
@@ -3,12 +3,18 @@ let mainScreen = document.querySelector(".mainScreen")
 let stopBtn = document.querySelector(".stopbtn")
 
 
+// variables to hold time values
 
-let storedTime = 00;
-let miniute =00;
-let hours = 00;
+// let mainObj = {
+//     storedTime: 00,
+//     miniute :00,
+//     hours:00
+// }
+   let storedTime = 00;
+   let miniute = 00;
+   let hours = 00;
 
-
+// our Timeincrement function in seconds 
 let incSeconds = ()=> {
     storedTime++
     if (storedTime > 59 ){
@@ -23,20 +29,8 @@ let incSeconds = ()=> {
         }
     }
 }
-let increaseSec = ()=> {
-    setInterval(incSeconds, 1000)
-}
-increaseSec();
 
-let secsPlaceBo = 0;
-let minsPlacebo = 0;
-let hoursPlacebo=0;
-let display = ()=> {
-   placeboCheck();
-  mainScreen.innerHTML= "<h1>"+hoursPlacebo+hours+":"+minsPlacebo+miniute+":"+secsPlaceBo+storedTime+"</h1>";
-
-} 
-
+// this puts a placebo value in front of the time values till they are double digits
 let placeboCheck = ()=> {
     if (storedTime >=0 && storedTime < 10 ){
         secsPlaceBo =0;
@@ -55,4 +49,34 @@ let placeboCheck = ()=> {
         hoursPlacebo= "";
     }
 }
-setInterval(display, 450)
+
+
+let secsPlaceBo = 0;
+let minsPlacebo = 0;
+let hoursPlacebo=0;
+
+// function allows us to display the change & furthe down, this is being diplayed every 450ms so our values adjust.
+let display = ()=> {
+  placeboCheck();
+  mainScreen.innerHTML= "<h1>"+hoursPlacebo+hours+":"+minsPlacebo+miniute+":"+secsPlaceBo+storedTime+"</h1>";
+
+} 
+
+let increaseSec = ()=> {
+    setInterval(incSeconds, 1150);
+    setInterval(display, 1000)
+}
+
+let addEventi = ()=> {
+   startBtn.addEventListener("click", increaseSec, {once: true});
+}
+
+addEventi();
+
+let pauseFunc = ()=> {
+    startBtn.removeEventListener("click",addEventi)
+    placeboCheck();
+  mainScreen.innerHTML= "<h1>"+hoursPlacebo+hours+":"+minsPlacebo+miniute+":"+secsPlaceBo+storedTime+"</h1>";
+}
+
+stopBtn.addEventListener("click", pauseFunc, {once: true})
