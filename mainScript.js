68e6bb7f96911fc4c7b0e6a2bb0e42247e9e70e9
Daprojects1 <93226161+Daprojects1@users.noreply.github.com>
2022-01-07 12:21:19 +0000

    Update mainScript.js
    added more functional programming

--- mainScript.js
+++ mainScript.js
@@ -1,28 +1,58 @@
 let startBtn = document.querySelector(".startbtn")
 let mainScreen = document.querySelector(".mainScreen")
+let stopBtn = document.querySelector(".stopbtn")
 
-// let showZeros = ()=> {
-//     mainScreen.innerHTML= "<h1 >00</h1>"
-// }
-// showZeros();
 
-let startTime = ()=> {}
-let storedTime = 0;
-let miniute =10;
-let hours = 0;
-let countTime = ()=> {
-    if (storedTime >= 0 && storedTime < 10){
-        mainScreen.innerHTML= "<h1>"+"00:0"+miniute+":0"+storedTime +"</h1>";
-        storedTime++;
-    } else if (storedTime >= 10 && storedTime < 60){
-        mainScreen.innerHTML = "<h1>"+"00:"+miniute+":"+storedTime +"</h1>";
-        storedTime++;
-    } else if(storedTime === 60 ){
-       storedTime=0;
-       miniute++;     
-    } 
-    
+
+let storedTime = 00;
+let miniute =00;
+let hours = 00;
+
+
+let incSeconds = ()=> {
+    storedTime++
+    if (storedTime > 59 ){
+        storedTime = 0
+        miniute++;
+        if (miniute > 59){
+            miniute = 0
+            hours++;
+        }
+        if (hours === 24){
+            hours = 0;
+        }
+    }
 }
-setInterval(countTime, 1000);
+let increaseSec = ()=> {
+    setInterval(incSeconds, 1000)
+}
+increaseSec();
 
+let secsPlaceBo = 0;
+let minsPlacebo = 0;
+let hoursPlacebo=0;
+let display = ()=> {
+   placeboCheck();
+  mainScreen.innerHTML= "<h1>"+hoursPlacebo+hours+":"+minsPlacebo+miniute+":"+secsPlaceBo+storedTime+"</h1>";
 
+} 
+
+let placeboCheck = ()=> {
+    if (storedTime >=0 && storedTime < 10 ){
+        secsPlaceBo =0;
+    } else if (storedTime >= 10){
+        secsPlaceBo="";
+    }
+    if (miniute >= 0 && miniute < 10){
+       minsPlacebo=0;
+
+    }else if (miniute >=10){
+        minsPlacebo=""
+    }
+    if (hours >= 0 && hours < 10 ){
+        hoursPlacebo= 0;
+    } else {
+        hoursPlacebo= "";
+    }
+}
+setInterval(display, 450)
