2cd6b02b30057293b211b70487f0e94561749397
Daprojects1 <93226161+Daprojects1@users.noreply.github.com>
2022-01-09 19:02:12 +0000

    Clear interval to stop the time from overrunning
    does this so that over-pressing start does not overly add more to the clock time.

--- mainScript.js
+++ mainScript.js
@@ -2,33 +2,29 @@ let startBtn = document.querySelector(".startbtn")
 let mainScreen = document.querySelector(".mainScreen")
 let stopBtn = document.querySelector(".stopbtn")
 let clearBtn = document.querySelector(".clearbtn")
-
-// variables to hold time values
-
-// let mainObj = {
-//     storedTime: 00,
-//     miniute :00,
-//     hours:00
-// }
+let newInt;
+// variables to hold time values 
    let storedTime = 0;
    let miniute = 0;
    let hours = 0;
 
 // our Timeincrement function in seconds 
-let incSeconds = ()=> {
-    storedTime++
-    if (storedTime > 59 ){
-        storedTime = 0
-        miniute++;
-        if (miniute > 59){
-            miniute = 0
-            hours++;
-        }
-        if (hours === 24){
-            hours = 0;
-        }
-    }
-}
+    let incSeconds = ()=> {
+        storedTime++;
+      if (storedTime > 59 ){
+          storedTime = 0
+          miniute++;
+          if (miniute > 59){
+              miniute = 0
+              hours++;
+          }
+          if (hours === 24){
+              hours = 0;
+          }
+      }
+  
+  }
+ 
 
 // this puts a placebo value in front of the time values till they are double digits
 let placeboCheck = ()=> {
@@ -50,25 +46,25 @@ let placeboCheck = ()=> {
     }
 }
 
+// placebo values 
 
 let secsPlaceBo = 0;
 let minsPlacebo = 0;
 let hoursPlacebo=0;
 
-// function allows us to display the change & further down, this is being diplayed every 750ms so our values adjust.
+// function allows us to display the change & further down, this is being displayed every 750ms so our values adjust Also has a placebo check & time inc function.
 let display = ()=> {
   placeboCheck();
   mainScreen.innerHTML= "<h1>"+hoursPlacebo+hours+":"+minsPlacebo+miniute+":"+secsPlaceBo+storedTime+"</h1>";
   incSeconds();
 } 
 
-// let increaseSec = ()=> {
-//     setInterval(incSeconds, 1500);
-// }
+// displayShow sets interval and has event listeners to stop and clear mainscreen.
 
 let displayShow= ()=> {
-   let newInt = setInterval(display, 1000)
-   stopBtn.addEventListener("click", ()=> {
+     clearInterval(newInt)
+     newInt = setInterval(display, 1000)
+     stopBtn.addEventListener("click", ()=> {
        clearInterval(newInt)
    })
    clearBtn.addEventListener("click", ()=> {
@@ -80,6 +76,7 @@ let displayShow= ()=> {
    })
 }
 
+// event listners for the start btn.  
+startBtn.addEventListener("click", displayShow);
 
- startBtn.addEventListener("click", displayShow);
 
